0634ea42e7e4f28e5819652501ec6ecd5db28c2c
Karolfc <karolfuentescaro@gmail.com>
2023-10-29 19:49:52 -0500

    Ya funciona el carrusel VAMOOOOOO

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -5,41 +5,45 @@ import { BsFillArrowRightCircleFill, BsFillArrowLeftCircleFill } from 'react-ico
 export function Carrusel ({ slides }) {
   const [current, setCurrent] = useState(0)
 
+  // const previousSlide = () => {
+  //   if (current === 0) setCurrent(slides.lenght - 1)
+  //   else setCurrent(current - 1)
+  // }
+
+  // const nextSlide = () => {
+  //   if (current === slides.lenght - 1) setCurrent(0)
+  //   else setCurrent(current + 1)
+  // }
+
   const previousSlide = () => {
-    if (current === 0) setCurrent(slides.lenght - 1)
-    else setCurrent(current - 1)
+    setCurrent(current === 0 ? slides.length - 1 : current - 1)
   }
 
   const nextSlide = () => {
-    if (current === slides.lenght - 1) setCurrent(0)
-    else setCurrent(current + 1)
+    setCurrent(current === slides.length - 1 ? 0 : current + 1)
   }
 
   return (
     <div className='overflow-hidden relative'>
       <div
-        className='flex transition ease-out duration-400'
+        className='flex transition ease-out duration-500'
         style={{
           transform: `translateX(-${current * 100}%`
         }}
       >
         {slides.map((s, index) => {
           return (
-            <div key={index} className='w-full'>
-              <img src={s.img} alt={`Image of ${s.member}`} />
-              <h3 className='text-xl font-semibold'>{s.member}</h3>
-              <p className='text-sm'>{s.rol}</p>
-              <p className='text-sm'>{s.intro}</p>
-            </div>
+            <img className='carrusel-Img' key={index} src={s} />
+
           )
         })}
       </div>
 
-      <div className='absolute top-0 h-full w-full justify-between item-center flex text-black px-10 text-3xl'>
-        <button onClick={previousSlide}>
+      <div className='absolute top-0 h-full w-full justify-between item-center flex text-white px-10 text-5xl py-10px' id='Btn-arrow'>
+        <button onClick={previousSlide} className='opacity-50 hover:opacity-100'>
           <BsFillArrowLeftCircleFill />
         </button>
-        <button onClick={nextSlide}>
+        <button onClick={nextSlide} className='opacity-50 hover:opacity-100'>
           <BsFillArrowRightCircleFill />
         </button>
       </div>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,28 +4,28 @@ import { Nav, Banner, Carrusel, FeaturedProjects, ContactSection, Footer } from 
 
 export function HomePage () {
   const slides = [
-    {
-      member: 'Jhohanson Jovel',
-      rol: 'Full-stack Developer',
-      intro: 'Im Jhohanson, I love videogames, programming, design and animation...',
-      img: './src/images/JhohanNBG.png',
-      bg: './src/images/fondoEquipo.jpg'
-    },
-    {
-      member: 'Karol Fuentes',
-      rol: 'Graphics and UI Designer',
-      intro: 'Im Karol, a passionate web designer and illustrator who loves creating...',
-      img: './src/images/KarolNBG.png'
-    },
-    {
-      member: 'Alexander Rueda',
-      rol: 'Full-stack Developer',
-      intro: 'Im Alex, a passionate about videogames, programming and design...',
-      img: './src/images/AlexNBG.png'
-    }
-    // './src/images/JhohanNBG.png',
-    // './src/images/KarolNBG.png',
-    // './src/images/AlexNBG.png'
+    // {
+    //   member: 'Jhohanson Jovel',
+    //   rol: 'Full-stack Developer',
+    //   intro: 'Im Jhohanson, I love videogames, programming, design and animation...',
+    //   img: './src/images/JhohanNBG.png',
+    //   bg: './src/images/fondoEquipo.jpg'
+    // },
+    // {
+    //   member: 'Karol Fuentes',
+    //   rol: 'Graphics and UI Designer',
+    //   intro: 'Im Karol, a passionate web designer and illustrator who loves creating...',
+    //   img: './src/images/KarolNBG.png'
+    // },
+    // {
+    //   member: 'Alexander Rueda',
+    //   rol: 'Full-stack Developer',
+    //   intro: 'Im Alex, a passionate about videogames, programming and design...',
+    //   img: './src/images/AlexNBG.png'
+    // }
+    './src/images/JhohanNBG.jpg',
+    './src/images/KarolNBG.jpg',
+    './src/images/AlexNBG.jpg'
   ]
 
   return (
@@ -33,7 +33,7 @@ export function HomePage () {
       <section>
         <Nav />
         <Banner />
-        <div className='w-[35%] m-auto mt-10'>
+        <div className='w-[100%] m-auto mt-10 '>
           <Carrusel slides={slides} />
         </div>
         <FeaturedProjects />
